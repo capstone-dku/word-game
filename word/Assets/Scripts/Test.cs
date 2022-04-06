bad1f6989c9faf3c479f5b1b6263bcd473d276ff
virtus2 <khd1326@naver.com>
2022-04-06 14:23:49 +0900

    Create test script

---
+++ word/Assets/Scripts/Test.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    private void Start()
+    {
+        Debug.Log("TEST");
+    }
+}
