e0c7f4fc48fe62a63f1ab6e84ab3ad594818fbb4
virtus2 <khd1326@naver.com>
2022-04-19 01:58:34 +0900

    깜빡이 단어 학습의 단어 세트 선택기 추가

---
+++ word/Assets/Scripts/VocaSelector.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VocaSelector : MonoBehaviour
+{
+    // 특정 difficulty, level의 단어를 json에서 가져온다.
+    public List<Voca> SelectVoca(int difficulty, int level)
+    {
+        List<Voca> voca = null;
+
+        return voca;
+    }
+}
