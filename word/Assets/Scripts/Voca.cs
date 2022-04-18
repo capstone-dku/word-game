375fdabee1e5cd9c12f21e8c0d97f345b25866e2
virtus2 <khd1326@naver.com>
2022-04-19 01:58:53 +0900

    단어 클래스 추가

---
+++ word/Assets/Scripts/Voca.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Voca : MonoBehaviour
+{
+    public int num;
+    public string voca;
+    public string meaning;
+    public int difficulty;
+}
