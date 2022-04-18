0d08ce328c9739e4804af9a264c78eddd5017abd
virtus2 <khd1326@naver.com>
2022-04-19 01:56:30 +0900

    깜빡이 단어 학습 UI 구현중

--- word/Assets/Scripts/VocaStudy.cs
+++ word/Assets/Scripts/VocaStudy.cs
@@ -2,20 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 단어 학습 인터페이스와 학습을 맡는 클래스
 public class VocaStudy : MonoBehaviour
 {
     [SerializeField]
-    private GameObject difficultyPanel; // 난이도 선택 패널창
+    private VocaSelector vocaSelector; 
+    [SerializeField]
+    private GameObject panelDifficulty; // 난이도 선택 패널창
+    [SerializeField]
+    private GameObject[] panelVocaSet = new GameObject[4]; // 세트 선택 패널창
 
-
-    void StartStudy()
+    /// <summary>
+    /// 학습 버튼 눌렀을 때 호출되는 함수.
+    /// 난이도 선택창을 연다.
+    /// </summary>
+    private void ShowPanelDifficulty()
     {
-        difficultyPanel.SetActive(true);
+        // 난이도 선택창을 보여준다.
+        panelDifficulty.SetActive(true);
         
     }
-
-    void SelectDiffculty()
+    /// <summary>
+    /// 닫기 버튼 눌렀을 때 호출되는 함수
+    /// 패널 창을 닫는다.
+    /// </summary>
+    /// <param name="go">닫을 패널 창</param>
+    public void OnButtonClickedClose(GameObject go)
     {
+        go.SetActive(false);
+    }
 
+    /// <summary>
+    /// 초,중,고,토익 버튼 눌렀을 때 호출되는 함수. 단어 세트 선택 창을 연다.
+    /// </summary>
+    /// <param name="diff">0: 초등, 1: 중등, 2: 고등, 3: 토익</param>
+    public void OnButtonClickDifficulty(int diff)
+    {
+        ShowPanelVocaSet(diff);
+    }
+
+    public void ShowPanelVocaSet(int i)
+    {
+        panelVocaSet[i].SetActive(true);
     }
 }
