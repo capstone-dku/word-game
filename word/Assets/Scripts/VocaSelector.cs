0f889bf04850b6c675028a16d512b5d35a1bed7f
virtus2 <khd1326@naver.com>
2022-04-22 23:55:33 +0900

    주석 수정

--- word/Assets/Scripts/VocaSelector.cs
+++ word/Assets/Scripts/VocaSelector.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class VocaSelector : MonoBehaviour
-{
-    // 특정 difficulty, level의 단어를 json에서 가져온다.
+{    
+    // TODO: 특정 difficulty, level의 단어를 단어장 json 파일에서 가져온다.
     public List<Voca> SelectVoca(int difficulty, int level)
     {
         List<Voca> voca = null;
