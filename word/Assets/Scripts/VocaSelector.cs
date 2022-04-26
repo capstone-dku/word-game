b02c54590fad997d59a71291d388f5e78b6a15f8
virtus2 <khd1326@naver.com>
2022-04-26 23:42:17 +0900

    단어 20개씩만 가져오게 수정

--- word/Assets/Scripts/VocaSelector.cs
+++ word/Assets/Scripts/VocaSelector.cs
@@ -9,11 +9,11 @@ public class VocaSelector : MonoBehaviour
     // TODO: 특정 difficulty, level의 단어를 단어장 json 파일에서 가져온다.
     public List<Voca> SelectVoca(int difficulty, int level)
     {
-        List<Voca> voca = JsonLoad(difficulty).GetRange(level*20,level*20+20);
+        List<Voca> voca = JsonLoad(difficulty).GetRange(level*20,20);
         
         return voca;
     }
-
+    
     /// <summary>
     /// 특정 diffculty의 set 갯수를 반환한다.
     /// </summary>
