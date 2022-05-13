f1b045b5ab60f7b11e2a2b112a2505a61f5a468d
virtus2 <khd1326@naver.com>
2022-05-14 00:34:59 +0900

    Update GridMap.cs

--- word/Assets/Scripts/GridMap.cs
+++ word/Assets/Scripts/GridMap.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Tilemaps;
