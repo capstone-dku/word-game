73e8c205a1e9b69d784c0db405193a8c40b694c1
virtus2 <khd1323@naver.com>
2022-10-12 16:12:41 +0900

    낱말퍼즐 키보드 구현

--- word/Assets/Scripts/UI/CrossWord/PanelCrossWord.cs
+++ word/Assets/Scripts/UI/CrossWord/PanelCrossWord.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PanelCrossWord : MonoBehaviour
 {
-
+    [SerializeField] private GameObject panelKeyboard;
     private List<Voca> vocaList; // 퍼즐판에 출제될 단어 리스트
     public void Init(List<Voca> vocaList)
     {
@@ -15,5 +15,14 @@ public class PanelCrossWord : MonoBehaviour
 
     }
 
+    private void ShowKeyboard(bool show)
+    {
+        panelKeyboard.SetActive(show);
+    }
+
+    public void OnClickedKeyboard(GameObject button)
+    {
+        Debug.Log(button.name);
+    }
 
 }
